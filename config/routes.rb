19f0bf6a3bcdf29dab6b2c948c72a55308294c8d
ayk-trsk <1219noppoppo@gmail.com>
2021-09-20 15:22:14 +0000

    [Add] コントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'homes/top'
+  resources :categories, except: [:new]
+  resources :titles
+  resources :categories, except: [:index]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
