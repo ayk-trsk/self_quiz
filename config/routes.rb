7cab567357c1d4223debca60c086214e864ec9a3
ayk-trsk <1219noppoppo@gmail.com>
2021-09-22 21:25:25 +0000

    [Fix]ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'homes/top'
-  resources :categories, except: [:new]
+  resources :categories, except: [:new, :show]
   resources :titles
-  resources :categories, except: [:index]
+  resources :quizzes, except: [:index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
